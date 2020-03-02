5ef921c908570e5c0eacb5d8dff160eb1f1e17d5
KyleSalisbury <60905147+KyleSalisbury@users.noreply.github.com>
2020-03-02 08:21:42 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 from urllib.request import urlretrieve
+import re
+from collections import Counter
 
 Address = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 
 Local_copy = 'Log_copy.log'
 
 local_file, headers = urlretrieve(Address, Local_copy) 
+
 
 #////////
 Local_copy = open('Log_copy.log')
@@ -16,3 +19,8 @@ checklist = ()
 LogTotal = 0
 
 Files = {}
+
+failcount = 0
+redirectcount = 0
+
+datecheck = ''
