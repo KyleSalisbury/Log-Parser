2878b6645069cbbc46f0dfa522c9fbfc669c1e1b
KyleSalisbury <60905147+KyleSalisbury@users.noreply.github.com>
2020-03-02 08:13:27 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,3 +5,10 @@ Address = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 Local_copy = 'Log_copy.log'
 
 local_file, headers = urlretrieve(Address, Local_copy) 
+
+#////////
+Local_copy = open('Log_copy.log')
+
+lines = Local_copy.readlines()
+
+checklist = ()
